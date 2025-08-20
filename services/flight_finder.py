0383acd920c1f467e964bbca32b46674a2351580
Mohammad <vambire02@yahoo.com>
2025-08-20 10:23:41 +0200

    Refactor flight data saving logic by removing unnecessary conditional checks

--- services/flight_finder.py
+++ services/flight_finder.py
@@ -56,8 +56,7 @@ class FlightFinderService:
                     flights.append(flight)
 
         logger.info(f'Found {len(flights)} possible flights from {airport} to departure airports')
-        if save_data:
-            data_manager.add_possible_flights(flights, save_data)
+        data_manager.add_possible_flights(flights, save_data)
         return flights
 
     def find_possible_roundtrip_flights_from_departure_airports(self, save_data=True):
@@ -145,8 +144,7 @@ class FlightFinderService:
         logger.info(f'Found {len(flights)} possible flights (direct and one-stop) from departure airports to destination '
                     f'airports with max stops: {max_stops}')
         logger.info(f'Estimated scraping time: {estimated_time}')
-        if save_data:
-            data_manager.add_possible_flights(flights, save_data)
+        data_manager.add_possible_flights(flights, save_data)
         return flights
 
     def get_estimated_checking_time(self, possible_flights):
